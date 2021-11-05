c970cf50d42680871b312672ba32b6ae0ff5c622
pascolithe <theo.pascoli@rte-france.com>
2021-11-05 16:17:04 +0100

    minor modif

--- src/main/java/com/faraocommunity/farao/gridcapajoblauncher/JobController.java
+++ src/main/java/com/faraocommunity/farao/gridcapajoblauncher/JobController.java
@@ -2,10 +2,8 @@ package com.faraocommunity.farao.gridcapajoblauncher;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -16,11 +14,9 @@ public class JobController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(JobController.class);
 
-    @Value("${task.manager.url")
-    private String taskManagerUrl;
-
-    @RequestMapping(value = "/jobs/{id}", method = RequestMethod.POST)
+    @PostMapping(value = "/jobs/{id}")
     public void launchJob(@PathVariable int id) {
-        LOGGER.info("Received order to launch task n°" + id);
+        LOGGER.info("Received order to launch task numero {}", id);
     }
+
 }
