c2e83f055045c327b13067fc87cba635f8c8c85f
Montigny <alexandre.montigny@rte-france.com>
2021-11-15 18:10:59 +0100

    launchJob method - WIP

--- src/main/java/com/faraocommunity/farao/gridcapajoblauncher/JobController.java
+++ src/main/java/com/faraocommunity/farao/gridcapajoblauncher/JobController.java
@@ -1,16 +1,17 @@
 package com.faraocommunity.farao.gridcapajoblauncher;
 
-import org.apache.commons.httpclient.util.URIUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.time.ZonedDateTime;
 
 /**
  * @author Alexandre Montigny {@literal <alexandre.montigny at rte-france.com>}
@@ -27,8 +28,21 @@ public class JobController {
     }
 
     @PostMapping(value = "/start/{id}")
-    public void launchJob(@PathVariable String id) {
+    public void launchJob(@PathVariable String id) throws IOException {
         LOGGER.info("Received order to launch task {}", id);
+        WebClient client = WebClient.create(jobLauncherConfigurationProperties.getTaskManagerUrlProperties().getTaskManagerUrl());
+        WebClient.UriSpec<WebClient.RequestBodySpec> uriSpec = client.post();
+        WebClient.RequestBodySpec bodySpec = uriSpec.uri("tasks/");
+        WebClient.RequestHeadersSpec<?> headersSpec = bodySpec.bodyValue(id);
+        WebClient.ResponseSpec responseSpec = headersSpec.header(
+                        HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML)
+                .acceptCharset(StandardCharsets.UTF_8)
+                .ifNoneMatch("*")
+                .ifModifiedSince(ZonedDateTime.now())
+                .retrieve();
+        responseSpec.bodyToMono(String.class).subscribe();
+        LOGGER.info("Post request sent");
     }
 
 }
