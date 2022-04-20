d01523e68d380f77acdf6bc0e57aec4bcb77156f
Joris Mancini <joris.mancini@rte-france.com>
2022-04-20 15:54:43 +0200

    Add status change of the task in manual run

--- src/main/java/com/farao_community/farao/gridcapa/job_launcher/JobLauncherController.java
+++ src/main/java/com/farao_community/farao/gridcapa/job_launcher/JobLauncherController.java
@@ -6,12 +6,8 @@
  */
 package com.farao_community.farao.gridcapa.job_launcher;
 
-import com.farao_community.farao.gridcapa.task_manager.api.TaskDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.slf4j.MDC;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,40 +20,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class JobLauncherController {
     private static final Logger LOGGER = LoggerFactory.getLogger(JobLauncherController.class);
 
-    private final RestTemplateBuilder restTemplateBuilder;
     private final JobLauncherService jobLauncherService;
-    private final JobLauncherConfigurationProperties jobLauncherConfigurationProperties;
 
-    public JobLauncherController(RestTemplateBuilder restTemplateBuilder,
-                                 JobLauncherService jobLauncherService,
-                                 JobLauncherConfigurationProperties jobLauncherConfigurationProperties) {
-        this.restTemplateBuilder = restTemplateBuilder;
+    public JobLauncherController(JobLauncherService jobLauncherService) {
         this.jobLauncherService = jobLauncherService;
-        this.jobLauncherConfigurationProperties = jobLauncherConfigurationProperties;
     }
 
     @PostMapping(value = "/start/{timestamp}")
     public ResponseEntity<Void> launchJob(@PathVariable String timestamp) {
-        LOGGER.info("Received order to launch task {}", timestamp);
-        String requestUrl = jobLauncherConfigurationProperties.getUrl().getTaskManagerTimestampUrl() + timestamp;
-        LOGGER.info("Requesting URL: {}", requestUrl);
-        ResponseEntity<TaskDto> responseEntity = restTemplateBuilder.build().getForEntity(requestUrl, TaskDto.class);
-        TaskDto taskDto = responseEntity.getBody();
-        if (taskDto != null) {
-            String taskId = taskDto.getId().toString();
-            // propagate in logs MDC the task id as an extra field to be able to match microservices logs with calculation tasks.
-            // This should be done only once, as soon as the information to add in mdc is available.
-            MDC.put("gridcapa-task-id", taskId);
-
-            if (responseEntity.getStatusCode() == HttpStatus.OK) {
-                jobLauncherService.launchJob(taskDto);
-                return ResponseEntity.ok().build();
-            } else {
-                return getEmptyResponseEntity(timestamp);
-            }
-        } else {
-            return getEmptyResponseEntity(timestamp);
+        if (jobLauncherService.launchJob(timestamp)) {
+            return ResponseEntity.ok().build();
         }
+        return getEmptyResponseEntity(timestamp);
     }
 
     private ResponseEntity<Void> getEmptyResponseEntity(@PathVariable String timestamp) {
--- src/main/java/com/farao_community/farao/gridcapa/job_launcher/JobLauncherService.java
+++ src/main/java/com/farao_community/farao/gridcapa/job_launcher/JobLauncherService.java
@@ -10,8 +10,14 @@ import com.farao_community.farao.gridcapa.task_manager.api.TaskDto;
 import com.farao_community.farao.gridcapa.task_manager.api.TaskStatus;
 
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.Objects;
 
@@ -20,24 +26,59 @@ import java.util.Objects;
  */
 @Service
 public class JobLauncherService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(JobLauncherService.class);
     private static final String RUN_BINDING = "run-task";
 
+    private final JobLauncherConfigurationProperties jobLauncherConfigurationProperties;
+    private final RestTemplateBuilder restTemplateBuilder;
     private final StreamBridge streamBridge;
     private final Logger jobLauncherEventsLogger;
 
-    public JobLauncherService(StreamBridge streamBridge, Logger jobLauncherEventsLogger) {
+    public JobLauncherService(JobLauncherConfigurationProperties jobLauncherConfigurationProperties,
+                              RestTemplateBuilder restTemplateBuilder, StreamBridge streamBridge,
+                              Logger jobLauncherEventsLogger) {
+        this.jobLauncherConfigurationProperties = jobLauncherConfigurationProperties;
+        this.restTemplateBuilder = restTemplateBuilder;
         this.streamBridge = streamBridge;
         this.jobLauncherEventsLogger = jobLauncherEventsLogger;
     }
 
-    public void launchJob(TaskDto taskDto) {
-        if (taskDto.getStatus() == TaskStatus.READY
-            || taskDto.getStatus() == TaskStatus.SUCCESS
-            || taskDto.getStatus() == TaskStatus.ERROR) {
-            jobLauncherEventsLogger.info("Task launched on TS {}", taskDto.getTimestamp());
-            streamBridge.send(RUN_BINDING, Objects.requireNonNull(taskDto));
-        } else {
-            jobLauncherEventsLogger.warn("Failed to launch task with timestamp {} because it is not ready yet", taskDto.getTimestamp());
+    public boolean launchJob(String timestamp) {
+        LOGGER.info("Received order to launch task {}", timestamp);
+        String requestUrl = getUrlToRetrieveTaskDto(timestamp);
+        LOGGER.info("Requesting URL: {}", requestUrl);
+        ResponseEntity<TaskDto> responseEntity = restTemplateBuilder.build().getForEntity(requestUrl, TaskDto.class);
+        TaskDto taskDto = responseEntity.getBody();
+        if (taskDto != null) {
+            String taskId = taskDto.getId().toString();
+            // propagate in logs MDC the task id as an extra field to be able to match microservices logs with calculation tasks.
+            // This should be done only once, as soon as the information to add in mdc is available.
+            MDC.put("gridcapa-task-id", taskId);
+
+            if (responseEntity.getStatusCode() == HttpStatus.OK) {
+                restTemplateBuilder.build().put(getUrlToUpdateTaskStatusToPending(timestamp), TaskDto.class);
+                if (taskDto.getStatus() == TaskStatus.READY
+                    || taskDto.getStatus() == TaskStatus.SUCCESS
+                    || taskDto.getStatus() == TaskStatus.ERROR) {
+                    jobLauncherEventsLogger.info("Task launched on TS {}", taskDto.getTimestamp());
+                    streamBridge.send(RUN_BINDING, Objects.requireNonNull(taskDto));
+                } else {
+                    jobLauncherEventsLogger.warn("Failed to launch task with timestamp {} because it is not ready yet", taskDto.getTimestamp());
+                }
+                return true;
+            }
         }
+        return false;
+    }
+
+    String getUrlToRetrieveTaskDto(String timestamp) {
+        return jobLauncherConfigurationProperties.getUrl().getTaskManagerTimestampUrl() + timestamp;
+    }
+
+    String getUrlToUpdateTaskStatusToPending(String timestamp) {
+        String url = jobLauncherConfigurationProperties.getUrl().getTaskManagerTimestampUrl() + timestamp + "/status";
+        UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(url)
+            .queryParam("status", TaskStatus.PENDING);
+        return builder.toUriString();
     }
 }
