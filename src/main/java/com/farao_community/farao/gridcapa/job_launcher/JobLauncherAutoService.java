bdb1f297c8f9986c49a4ac28b9ca3e820105cf2e
kahyami <50823693+kahyami@users.noreply.github.com>
2022-06-08 16:47:22 +0200

    automatic launch (#10)
    * launch task automatically when the status is ready
    
    * update task status to pending and precise the exchange name
    
    * update binding name
    
    * add a default group

---
+++ src/main/java/com/farao_community/farao/gridcapa/job_launcher/JobLauncherAutoService.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.farao_community.farao.gridcapa.job_launcher;
+
+import com.farao_community.farao.gridcapa.task_manager.api.TaskDto;
+import com.farao_community.farao.gridcapa.task_manager.api.TaskStatus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Flux;
+
+import java.util.Objects;
+import java.util.function.Consumer;
+
+/**
+ * @author Amira Kahya {@literal <amira.kahya at rte-france.com>}
+ */
+@Service
+@ConditionalOnProperty(name = "job-launcher.auto", havingValue = "true")
+public class JobLauncherAutoService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(JobLauncherAutoService.class);
+    private static final String RUN_BINDING = "run-task-auto";
+    private final Logger jobLauncherEventsLogger;
+    private final StreamBridge streamBridge;
+    private final JobLauncherConfigurationProperties jobLauncherConfigurationProperties;
+    private final RestTemplateBuilder restTemplateBuilder;
+
+    public JobLauncherAutoService(Logger jobLauncherEventsLogger, StreamBridge streamBridge, JobLauncherConfigurationProperties jobLauncherConfigurationProperties, RestTemplateBuilder restTemplateBuilder) {
+        this.jobLauncherEventsLogger = jobLauncherEventsLogger;
+        this.streamBridge = streamBridge;
+        this.jobLauncherConfigurationProperties = jobLauncherConfigurationProperties;
+        this.restTemplateBuilder = restTemplateBuilder;
+    }
+
+    @Bean
+    public Consumer<Flux<TaskDto>> consumeTaskDtoUpdate() {
+        return f -> f
+                .onErrorContinue((t, r) -> LOGGER.error(t.getMessage(), t))
+                .subscribe(this::runReadyTasks);
+    }
+
+    void runReadyTasks(TaskDto taskDtoUpdated) {
+        if (taskDtoUpdated.getStatus().equals(TaskStatus.READY)) {
+            jobLauncherEventsLogger.info("Task launched on TS {}", taskDtoUpdated.getTimestamp());
+            restTemplateBuilder.build().put(getUrlToUpdateTaskStatusToPending(taskDtoUpdated), TaskDto.class);
+            streamBridge.send(RUN_BINDING, Objects.requireNonNull(taskDtoUpdated));
+        }
+    }
+
+    private String getUrlToUpdateTaskStatusToPending(TaskDto taskDto) {
+        String url = jobLauncherConfigurationProperties.getUrl().getTaskManagerTimestampUrl() + taskDto.getTimestamp() + "/status";
+        UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(url)
+                .queryParam("status", TaskStatus.PENDING);
+        return builder.toUriString();
+    }
+}
