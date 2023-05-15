8a7fb9e7a265125752213ab2e8f0d18e94beac2c
SCHWITZGUEBEL Marc Ext <schwitzguebelmar@rte-france.com>
2023-05-15 10:28:15 +0200

    Adding licence and developper information to new test class

--- src/test/java/com/farao_community/farao/gridcapa/job_launcher/JobLauncherAutoServiceTest.java
+++ src/test/java/com/farao_community/farao/gridcapa/job_launcher/JobLauncherAutoServiceTest.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2023, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.farao_community.farao.gridcapa.job_launcher;
 
 import com.farao_community.farao.gridcapa.task_manager.api.TaskDto;
@@ -18,6 +24,9 @@ import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * @author Marc Schwitzguébel {@literal <marc.schwitzguebel at rte-france.com>}
+ */
 @SpringBootTest
 class JobLauncherAutoServiceTest {
 
